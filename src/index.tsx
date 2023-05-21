8aa0d9c271005dd15abc50c2f15a4153ba47e3d6
umkament <97944243+umkament@users.noreply.github.com>
2023-05-21 23:20:35 +0300

    четвергИтоговый 7 _

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client';
-import { Provider, TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import axios, { AxiosError } from 'axios';
-import { applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux';
-import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk';
+import { applyMiddleware, combineReducers, legacy_createStore as createStore, Dispatch } from 'redux'
+import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk'
+import { Provider, TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import axios, { AxiosError } from 'axios'
 
-
-// Types
-type NullableType<T> = null | T
-
-type LoginFieldsType = {
-  email: string
-  password: string
+// TYPES
+type TodoType = {
+  id: string;
+  title: string;
+  order: number;
+  createdAt: string;
+  updatedAt: string;
+  completed: boolean;
 }
+
+type UserType = {
+  id: string;
+  name: string;
+  age: number;
+}
+
+type UsersResponseType = {
+  items: UserType[]
+  totalCount: number
+}
+
 
 // API
 const instance = axios.create({baseURL: 'https://exams-frontend.kimitsu.it-incubator.ru/api/'})
 
 const api = {
-  login(data: LoginFieldsType) {
-    return instance.post('auth/login', data)
+  getTodos() {
+    return instance.get<TodoType[]>('todos')
+  },
+  getUsers() {
+    return instance.get<UsersResponseType>('users')
   },
 }
 
@@ -27,16 +43,19 @@ const api = {
 // Reducer
 const initState = {
   isLoading: false,
-  error: null as NullableType<string>,
-  isLoggedIn: false,
+  error: null as string | null,
+  todos: [] as TodoType[],
+  users: [] as UserType[],
 }
 
 type InitStateType = typeof initState
 
 const appReducer = (state: InitStateType = initState, action: ActionsType): InitStateType => {
   switch (action.type) {
-    case 'APP/SET-IS-LOGGED-IN':
-      return {...state, isLoggedIn: action.isLoggedIn}
+    case 'APP/GET-TODOS':
+      return {...state, todos: action.todos}
+    case 'APP/GET-USERS':
+      return {...state, users: action.users}
     case 'APP/IS-LOADING':
       return {...state, isLoading: action.isLoading}
     case 'APP/SET-ERROR':
@@ -46,23 +65,45 @@ const appReducer = (state: InitStateType = initState, action: ActionsType): Init
   }
 }
 
-// Actions
-const setIsLoggedIn = (isLoggedIn: boolean) => ({type: 'APP/SET-IS-LOGGED-IN', isLoggedIn} as const)
+const getUsersAC = (users: UserType[]) => ({type: 'APP/GET-USERS', users} as const)
+const getTodosAC = (todos: TodoType[]) => ({type: 'APP/GET-TODOS', todos} as const)
 const setLoadingAC = (isLoading: boolean) => ({type: 'APP/IS-LOADING', isLoading} as const)
 const setError = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
-type ActionsType = | ReturnType<typeof setIsLoggedIn> | ReturnType<typeof setLoadingAC> | ReturnType<typeof setError>
+
+type ActionsType =
+   | ReturnType<typeof getUsersAC>
+   | ReturnType<typeof getTodosAC>
+   | ReturnType<typeof setLoadingAC>
+   | ReturnType<typeof setError>
+
+
+// Utils functions
+function baseSuccessHandler<T>(dispatch: Dispatch, actionCreator: Function, data: T) {
+  dispatch(actionCreator(data))
+  dispatch(setLoadingAC(false))
+}
 
 // Thunk
-const loginTC = (values: LoginFieldsType): AppThunk => (dispatch) => {
+const getTodosTC = (): AppThunk => (dispatch) => {
   dispatch(setLoadingAC(true))
-  api.login(values)
+  api.getTodos()
      .then((res) => {
-       dispatch(setIsLoggedIn(true))
-       alert('Вы залогинились успешно')
+       // XXX
      })
-     .catch((e) => {
+     .catch((e: AxiosError) => {
+       dispatch(setError(e.message))
+       dispatch(setLoadingAC(false))
      })
-     .finally(() => {
+}
+
+const getUsersTC = (): AppThunk => (dispatch) => {
+  dispatch(setLoadingAC(true))
+  api.getUsers()
+     .then((res) => {
+       // YYY
+     })
+     .catch((e: AxiosError) => {
+       dispatch(setError(e.message))
        dispatch(setLoadingAC(false))
      })
 }
@@ -80,68 +121,95 @@ const useAppDispatch = () => useDispatch<AppDispatch>()
 const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 
+// COMPONENTS
 // Loader
 export const Loader = () => {
-  return <h1>Loading ...</h1>
+  return (
+     <h1>Loading ...</h1>
+  )
 }
 
-// App
-export const App = () => {
+const App = () => {
+  return (
+     <>
+       <h1>✅Todos & 🙂Users</h1>
+       <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+         <Todos/>
+         <Users/>
+       </div>
+     </>
+  )
+}
 
+const Todos = () => {
   const dispatch = useAppDispatch()
-
-  const [form, setForm] = useState<LoginFieldsType>({email: '', password: ''})
-
+  const todos = useAppSelector(state => state.app.todos)
   const error = useAppSelector(state => state.app.error)
   const isLoading = useAppSelector(state => state.app.isLoading)
 
-  const changeFormValuesHandler = (e: React.ChangeEvent<HTMLInputElement>, field: string) => {
-    if (field === 'email') {
-      setForm({...form, email: e.currentTarget.value})
-    }
-    if (field === 'password') {
-      setForm({...form, password: e.currentTarget.value})
-    }
-  };
-
-  const submitForm = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
-    dispatch(loginTC(form))
-  };
+  useEffect(() => {
+    dispatch(getTodosTC())
+  }, [])
 
   return (
      <div>
+       <h2>✅ Список тудулистов</h2>
        {!!error && <h2 style={{color: 'red'}}>{error}</h2>}
        {isLoading && <Loader/>}
-       <form>
-         <div>
-           <input placeholder={'Введите email'}
-                  value={form.email}
-                  onChange={(e) => changeFormValuesHandler(e, 'email')}
-           />
-         </div>
-         <div>
-           <input type={'password'}
-                  placeholder={'Введите пароль'}
-                  value={form.password}
-                  onChange={(e) => changeFormValuesHandler(e, 'password')}
-           />
-         </div>
-         <button type="submit" onClick={submitForm}>Залогиниться</button>
-       </form>
+       {
+         todos.map((t) => {
+           return (
+              <div style={t.completed ? {color: 'grey'} : {}} key={t.id}>
+                <input type="checkbox" checked={t.completed}/>
+                <b>Описание</b>: {t.title}
+              </div>
+           )
+         })
+       }
      </div>
-  );
+  )
+}
+
+const Users = () => {
+  const dispatch = useAppDispatch()
+  const users = useAppSelector(state => state.app.users)
+  const error = useAppSelector(state => state.app.error)
+  const isLoading = useAppSelector(state => state.app.isLoading)
+
+  useEffect(() => {
+    dispatch(getUsersTC())
+  }, [])
+
+  return (
+     <div>
+       <h2>🙂 Список юзеров</h2>
+       {!!error && <h2 style={{color: 'red'}}>{error}</h2>}
+       {isLoading && <Loader/>}
+       <div>
+         {
+           users.map(u => {
+             return (
+                <div key={u.id}>
+                  <b>name</b>:{u.name} - <b>age</b>:{u.age}
+                </div>
+             )
+           })
+         }</div>
+     </div>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<Provider store={store}> <App/></Provider>)
 
-// 📜 Описание:
-// Перед вами форма логинизации. Введите любые логин и пароль и попробуйте залогиниться.
-// У вас это навряд ли получится 😈, т.к. вы не знаете email и пароль.
-// Откройте Network и проанализируйте запрос.
-// Задача: вывести сообщение об ошибке, которую возвращает сервера говорящую о том что email или password некорректны.
 
-// В качестве ответа указать строку коду, которая позволит это осуществить.
-// 🖥 Пример ответа: dispatch('Error message')
-// ❗ Типизировать ошибку не надо, т.к. там есть много нюансов, о которых вы узнаете позже
+// 📜 Описание:
+// Перед вами список тудулистов и пользователей, которые находятся в постоянной загрузке.
+// Откройте network и вы увидите что запросы на сервер уходят и возвращаются с хорошими данными,
+// но вместо этого пользователь видит на экране Loader.
+// Для обработки успешного результата написана утилитная функция baseSuccessHandler.
+// Ваша задача воспользоваться этой функцией отобразить Todos и Users
+// Что нужно написать вместо XXX и YYY, чтобы реализовать данную задачу?
+// Ответ дайте через пробел.
+
+// 🖥 Пример ответа: dispatch(baseSuccessHandler(1,2,3))  dispatch(baseSuccessHandler(3,2,1)
