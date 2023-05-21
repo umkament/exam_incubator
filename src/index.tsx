7f00707ea14ac4f85aebe78ae105a822f0f8669e
umkament <97944243+umkament@users.noreply.github.com>
2023-05-21 23:31:32 +0300

    четвергИтоговый 9 _

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,73 @@
+import { useFormik } from 'formik';
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
-export const PageNotFound = () => {
-  return <h2>⛔ 404. Page not found ⛔</h2>
+// Types
+type LoginFieldsType = {
+  firstName: string
+  email: string
 }
 
-export const Profile = () => {
-  return <h2>😎 Профиль</h2>
-}
+// Main
+export const Login = () => {
 
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      email: '',
+    },
+    validate: (values) => {
+      const errors: Partial<LoginFieldsType> = {};
 
-export const Main = () => {
+      if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+      }
+      return errors
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+
+  // Функция необходима для того, чтобы вебшторм не ругался на true в JSX
+  const getTrue = () => {
+    return true
+  }
+
   return (
-     <>
-       <h2>✅ Список тудулистов</h2>
-       <h2>📜 Список постов</h2>
-     </>
-  )
+     <form onSubmit={formik.handleSubmit}>
+       <div>
+         <input placeholder={'Введите имя'} {...formik.getFieldProps('firstName')}/>
+       </div>
+       <div>
+         <input placeholder={'Введите email'}{...formik.getFieldProps('email')}/>
+         {getTrue() && <div style={{color: 'red'}}>ERROR</div>}
+       </div>
+       <button type="submit">Отправить</button>
+     </form>
+  );
 }
 
 // App
 export const App = () => {
-
   return (
      <Routes>
-       <Route path={'profile'} element={<Profile/>}/>
-       {<Route path={'*'} element={<Navigate to='/profile'/>}/>}
+       <Route path={''} element={<Login/>}/>
      </Routes>
   )
 }
-
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<BrowserRouter><App/></BrowserRouter>)
 
 // 📜 Описание:
-// Вместо ХХХ напишите роут таким образом, чтобы вне зависимости от того чтобы будет в урле (login или home или...)
-// вас всегда редиректило на страницу профиля и при в это в урле по итогу
-// был адрес /profile
+// Загрузив приложение вы увидите ошибку под полем email, но вы еще ничего не ввели.
+// Исправьте 46 строку кода так, чтобы:
+// 1) Сообщение об ошибке показывалось только в том случае, когда email введен некорректно.
+// 2) Вместо ERROR должен быть конкретный текст ошибки прописанный в валидации к этому полю.
+// 3) Сообщение должно показываться только в том случае, когда мы взаимодействовали с полем.
+// Исправленную версию строки напишите в качестве ответа.
 
-// 🖥 Пример ответа: <Route path={'/'} element={'to profile page'}/>
+// 🖥 Пример ответа: {true && <div style={{color: 'red'}}>error.email</div>}
