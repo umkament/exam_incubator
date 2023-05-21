1bbff4333443b701e71780000d3ab97cb88f068d
umkament <97944243+umkament@users.noreply.github.com>
2023-05-21 09:57:49 +0300

    четверг4/2 _

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export const Main = () => {
   return (
@@ -13,19 +13,21 @@ export const Main = () => {
 
 // App
 export const App = () => {
+
   return (
      <Routes>
-       <Route path={'/'} element={<Main/>}/>
+       <Route path={'/'} element={Main}/>
      </Routes>
   )
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<BrowserRouter><App/></BrowserRouter>)
 
 // 📜 Описание:
-// Белый экран... Приложение не работает.
+// Приложение при старте падает с ошибкой...
 // Найдите и исправьте ошибку, чтобы на экране отобразилось 2 заголовка.
 // Исправленную версию строки напишите в качестве ответа.
 
-// 🖥 Пример ответа: <Route path={'/'} component={<Main/>}/>
+// 🖥 Пример ответа: type InitStateType = typeof initState
