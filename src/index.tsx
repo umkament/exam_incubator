432a408f9a43b4fca4d3e1137b0203543d173503
umkament <97944243+umkament@users.noreply.github.com>
2023-07-17 13:35:38 +0300

    пятница 1/6

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,113 @@
 import ReactDOM from 'react-dom/client';
-import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes, useNavigate, useParams } from 'react-router-dom'
+import React from 'react'
 
-export const Mining = () => {
-  const [btc, setBtc] = useState(0)
+type UserType = {
+  id: number
+  name: string
+  avatar: string
+  age: number
+  address: string
+}
 
-  useEffect(() => {
-    setInterval(() => {
-       let prevState = 0
-      setBtc(prevState++)
-      // ❗❗❗ XXX ❗❗❗
-    }, 1000)
-  }, [])
+const users: UserType[] = [
+  {
+    id: 1,
+    name: 'my Name',
+    age: 32,
+    avatar: '—ฅ/ᐠ.̫ .ᐟ\\ฅ—',
+    address: 'my Address'
+  },
+  {
+    id: 2,
+    name: 'John',
+    age: 22,
+    avatar: ':)',
+    address: 'California'
+  },
+  {
+    id: 3,
+    name: 'Mike',
+    age: 18,
+    avatar: '^._.^',
+    address: 'New York'
+  },
+  {
+    id: 4,
+    name: 'Emma',
+    age: 38,
+    avatar: '/ᐠ-ꞈ-ᐟ\\',
+    address: 'Washington'
+  },
+]
+
+const StartPage = () => {
+  const navigate = useNavigate()
+  const friends = users.filter(u => u.id !== 1)
+
+  const mappedFriends = friends.map((f, i) => {
+    const go = () => {
+      navigate('/friend/' + f.id)
+    }
+
+    return (
+       <div key={i} onClick={go} style={{paddingLeft: 24, color: 'blue', cursor: 'pointer'}}>
+         {f.name}, {f.age}
+       </div>
+    )
+  })
 
   return (
-     <h1>🪙 BTC: {btc}</h1>
+     <div>
+       <h2>🙂 My profile</h2>
+       <Profile userId={1}/>
+       <hr/>
+       <h2>👪 Friends</h2>
+       {mappedFriends}
+     </div>
+  )
+}
+const Profile: React.FC<{ userId?: number }> = ({userId}) => {
+  const {id} = useParams<{ id: string }>()
+  const user = users.find(u => u.id === +(id || userId || 0))
+
+  return (
+     <div>
+       <div>
+         <b>avatar</b> {user?.avatar}
+       </div>
+       <div>
+         <div><b>name</b>: {user?.name}</div>
+         <div><b>age</b>: {user?.age}</div>
+         <div><b>address</b>: {user?.address}</div>
+       </div>
+     </div>
+  )
+}
+
+export const Friends = () => {
+  return (
+     <Routes>
+       <Route path={'/'} element={<StartPage/>}/>
+       <Route path={'friend'} element={<Profile/>}/>
+       <Route path={'*'} element={<div>❌404 Page Not Found❌</div>}/>
+     </Routes>
   )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<Mining/>);
+root.render(
+   <BrowserRouter>
+     <Friends/>
+   </BrowserRouter>
+);
 
 // 📜 Описание:
-// Помогите разработчику намайнить биткоинов.
-// Что-то не майнятся 😥.
-// Что необходимо написать вместо XXX чтобы биткоины майнились (каждую секунду прибавлялся 1 биткоин) ?
-// ❗В качестве переменной используйте значение prevState
-
-// 🖥 Пример ответа: btc = 1000000
+// При загрузке приложения на экране отображается
+// профиль пользователя и список друзей.
+// Если кликнуть на пользователя, то видим ❌404 Page Not Found❌
+// Исправьте код, чтобы по клику на пользователя
+// отображалась странице с информацией о друге.
+// В качестве ответа укажите исправленную строку кода.
+//
+// 🖥 Пример ответа: <Profile userId={4}/>
