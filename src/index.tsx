3f7ed3e93a059ac776438b0b0b03df0ac547a7d2
umkament <97944243+umkament@users.noreply.github.com>
2023-09-08 10:01:07 +0300

    пятница 1/5

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,96 @@
-import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Route, Routes, useNavigate, useParams} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate, useParams } from 'react-router-dom'
+import React from 'react'
 
-const Login = () => {
+type UserType = {
+  id: number
+  name: string
+  avatar: string
+  age: number
+  address: string
+}
+
+const users: UserType[] = [
+  {
+    id: 1,
+    name: 'my Name',
+    age: 32,
+    avatar: '—ฅ/ᐠ.̫ .ᐟ\\ฅ—',
+    address: 'my Address'
+  },
+  {
+    id: 2,
+    name: 'John',
+    age: 22,
+    avatar: ':)',
+    address: 'California'
+  },
+  {
+    id: 3,
+    name: 'Mike',
+    age: 18,
+    avatar: '^._.^',
+    address: 'New York'
+  },
+  {
+    id: 4,
+    name: 'Emma',
+    age: 38,
+    avatar: '/ᐠ-ꞈ-ᐟ\\',
+    address: 'Washington'
+  },
+]
+
+const StartPage = () => {
   const navigate = useNavigate()
+  const friends = users.filter(u => u.id !== 1)
 
-  useEffect(() => {
-    navigate('/secret/JIUzI1NiIsInR5cCI6IkpXVCJ9')
-  }, [])
+  const mappedFriends = friends.map((f, i) => {
+    const go = () => {
+      navigate('/friend/' + f.id)
+    }
+
+    return (
+       <div key={i} onClick={go} style={{paddingLeft: 24, color: 'blue', cursor: 'pointer'}}>
+         {f.name}, {f.age}
+       </div>
+    )
+  })
 
   return (
-     <div>Login</div>
+     <div>
+       <h2>🙂 My profile</h2>
+       <Profile userId={1}/>
+       <hr/>
+       <h2>👪 Friends</h2>
+       {mappedFriends}
+     </div>
   )
 }
-const SecretToken = () => {
-  const {token} = useParams() // FIX
+const Profile: React.FC<{ userId?: number }> = ({userId}) => {
+  const {id} = useParams<{ id: string }>()
+  const user = users.find(u => u.id === +(id || userId || 0))
 
   return (
-     <h1> token: {token}</h1>
+     <div>
+       <div>
+         <b>avatar</b> {user?.avatar}
+       </div>
+       <div>
+         <div><b>name</b>: {user?.name}</div>
+         <div><b>age</b>: {user?.age}</div>
+         <div><b>address</b>: {user?.address}</div>
+       </div>
+     </div>
   )
 }
 
-export const App = () => {
+export const Friends = () => {
   return (
      <Routes>
-       <Route path={'/'} element={<Login/>}/>
-       <Route path={'/secret/:token'} element={<SecretToken/>}/>
+       <Route path={'/'} element={<StartPage/>}/>
+       <Route path={'friend/:id'} element={<Profile/>}/>
+       <Route path={'*'} element={<div>❌404 Page Not Found❌</div>}/>
      </Routes>
   )
 }
@@ -33,13 +98,17 @@ export const App = () => {
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
    <BrowserRouter>
-     <App/>
+     <Friends/>
    </BrowserRouter>
 );
 
-// 📜Описание:
-// Исправьте код на 17 строке так, чтобы на странице отобразился токен.
-
-//❗Ответ можно указывать с типизацией и без. Учтено несколько вариантов
-// 🖥 Пример ответа: const token = '123'
-// const {token} = useParams() ответ засчитан
+// 📜 Описание:
+// При загрузке приложения на экране отображается
+// профиль пользователя и список друзей.
+// Если кликнуть на пользователя, то видим ❌404 Page Not Found❌
+// Исправьте код, чтобы по клику на пользователя
+// отображалась странице с информацией о друге.
+// В качестве ответа укажите исправленную строку кода.
+//
+// 🖥 Пример ответа: <Profile userId={4}/>
+// ответ <Route path={'friend/:id'} element={<Profile/>}/>  - засчитан
