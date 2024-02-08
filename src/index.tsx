dfeff44673f54a990c3565529b1c52dd1c042113
umkament <97944243+umkament@users.noreply.github.com>
2024-02-08 13:26:32 +0300

    пятница 3/1

--- src/index.tsx
+++ src/index.tsx
@@ -1,85 +1,72 @@
-import ReactDOM from 'react-dom/client';
-import React, { useEffect } from 'react';
-import { applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux'
-import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk'
-import { Provider, TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { Provider, TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 // Reducer
-const initState = {find: '', words: [] as string[]}
-type InitStateType = typeof initState
+const initState = {
+  animals: [
+    { likes: 0, name: "cat" },
+    { likes: 0, name: "dog" },
+    { likes: 0, name: "fish" },
+    { likes: 0, name: "spider" },
+    { likes: 0, name: "bird" },
+  ] as { likes: number; name: string }[],
+};
+type InitStateType = typeof initState;
 
 const appReducer = (state: InitStateType = initState, action: ActionsType): InitStateType => {
   switch (action.type) {
-    case 'SET_FIND':
-      return {...state, find: action.find}
-    case 'SET_WORDS':
-      return {...state, words: action.words}
-    default:
-      return state
+    case "LIKE":
+      return {
+        ...state,
+        animals: state.animals.map((animal) => {
+          return true ? { ...animal } : animal;
+        }),
+      };
   }
-}
+  return state;
+};
 
 // Store
-const rootReducer = combineReducers({app: appReducer})
+const rootReducer = combineReducers({ app: appReducer });
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
-type RootState = ReturnType<typeof store.getState>
-type AppDispatch = ThunkDispatch<RootState, unknown, ActionsType>
-type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, ActionsType>
-const useAppDispatch = () => useDispatch<AppDispatch>()
-const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+const store = configureStore({ reducer: rootReducer });
+type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = ThunkDispatch<RootState, unknown, ActionsType>;
+type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, ActionsType>;
+const useAppDispatch = () => useDispatch<AppDispatch>();
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-const setFind = (find: string) => ({type: 'SET_FIND', find} as const)
-const setWords = (words: string[]) => ({type: 'SET_WORDS', words} as const)
-type ActionsType = ReturnType<typeof setFind> | ReturnType<typeof setWords>
-
+const like = (likes: any, name: any) => ({ type: "LIKE", likes, name }) as const;
+type ActionsType = ReturnType<typeof like>;
 
 // Components
-const defWords = ['a', 'ab', 'abc', 'b', 'bc', 'c', 'd', 'ac', 'bcd', 'cd', 'abcd', 'bd']
+export const Animals = () => {
+  const animals = useAppSelector((state) => state.app.animals);
+  const dispatch = useAppDispatch();
 
-export const App = () => {
-  const find = useAppSelector(state => state.app.find)
-  const words = useAppSelector(state => state.app.words)
-
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(setWords(defWords))
-  }, [])
-
-  const mapped = words
-     .filter((w: string) => new RegExp(find, 'gi').test(w))
-     .map((w: string, i: number) => <div key={i}>{w}</div>)
-
-  const onChangeHandler = (value: string) => {
-    dispatch(setFind(value))
-    console.log(value)
-  }
-
-  return (
-     <div>
-       <input
-          value={find}
-          onChange={e => onChangeHandler(e.target.value)}
-       />
-       {mapped}
+  const mapped = animals.map((a: any, i: number) => (
+     <div key={i}>
+       {a.name}-{a.likes}-<button onClick={() => dispatch(like(a.likes + 1, a.name))}>Like!</button>
      </div>
-  )
-}
+  ));
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+  return <div>{mapped}</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
    <Provider store={store}>
-     <App/>
-   </Provider>
+     <Animals />
+   </Provider>,
 );
 
 // 📜 Описание:
-// На экране отображен массив слов.
-// Ваша задача починить фильтрацию:
-// вводите символы в input и сразу видите как фильтруются данные.
-// В качестве ответа укажите исправленную версию строки.
+// На экране отображен список животных.
+// Кликните на like и вы увидите, что ничего не происходит.
+// Ваша задача починить лайки.
+// В качестве ответа укажите исправленную версию строки
 //
-// 🖥 Пример ответа: dispatch(setFind(defWords))
-// ответ - dispatch(setFind(value)) - засчитан
+// 🖥 Пример ответа: -{a.likes + 1}-
